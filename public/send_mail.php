2ada1c3f00c6ea2ce4b712124ad3942131079289
Iago Cavalcante <iagoangelimc@gmail.com>
2018-03-20 16:03:28 -0300

    Lista de casamento e confirmação de presença

---
+++ public/send_mail.php
@@ -0,0 +1,19 @@
+<?php
+
+if(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['mensagem']) || empty($_POST['evento'])) {
+    ini_set('display_errors', 1);
+
+    $from = $_POST['email'];
+
+    $to = "karla.2.costa@gmail.com";
+
+    $subject = "Confirmação de Presença";
+
+    $message = $_POST['mensagem'];
+
+    $headers = "De:". $from;
+
+    mail($to, $subject, $message, $headers);
+}
+
+?>
