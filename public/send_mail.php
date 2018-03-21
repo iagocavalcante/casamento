de821063ac6f40916be92877fd2f3b8136e06e85
Iago Cavalcante <iagoangelimc@gmail.com>
2018-03-20 23:55:13 -0300

    Envio de email

--- public/send_mail.php
+++ public/send_mail.php
@@ -7,12 +7,14 @@ require 'vendor/autoload.php';
         //Server settings
         $mail->SMTPDebug = 0;                                 // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->isSendmail();
         $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
         $mail->Username = 'iagoangelimc@gmail.com';                 // SMTP username
         $mail->Password = 'cavalcante260412';                           // SMTP password
         $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 587;                                    // TCP port to connect to
+        $mail->CharSet = 'utf-8';
 
         //Recipients
         $mail->setFrom($_POST['email'], $_POST['nome']);
