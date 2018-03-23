0fff8bc0d094e85b442c35183d8c9b2783474cd0
Iago Cavalcante <iagoangelimc@gmail.com>
2018-03-23 11:06:59 -0300

    Email

--- public/send_mail.php
+++ public/send_mail.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
     $mail = new PHPMailer;                              // Passing `true` enables exceptions
     try {
         //Server settings
-        $mail->SMTPDebug = 1;                                 // Enable verbose debug output
+        $mail->SMTPDebug = 0;                                 // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';
 //        $mail->Host = 'ssl://smtp.gmail.com';  // Specify main and backup SMTP servers
